531974e7e29223088e98159f585eb8b0c939a4ea
ahadi15 <ahadimutua15@gmail.com>
2026-03-20 18:55:46 +0300

    add product inventory

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,28 @@
-// Write your code here
+
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+
+function logFirstProduct() {
+    console.log(products[0]);
+}
+
+
+function addProduct(productName) {
+    products.push(productName);
+}
+
+
+function updateProductName(index, newName) {
+    products[index] = newName;
+}
+
+
+function removeLastProduct() {
+    products.pop();
+}
 
 
 
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
+
+
+
